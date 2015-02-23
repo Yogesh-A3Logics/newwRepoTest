78d15e6ba9e1b9c9f9dc929eed396d65a3c077b1
Yogesh Singh Rana <yogeshsinghrana@Yogeshs-MacBook-Pro.local>
2015-02-24 00:15:56 +0530

    Initial Commit

---
+++ neww/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  neww
+//
+//  Created by Yogesh Singh Rana on 24/02/15.
+//  Copyright (c) 2015 Yogesh Singh Rana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ neww/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  neww
+//
+//  Created by Yogesh Singh Rana on 24/02/15.
+//  Copyright (c) 2015 Yogesh Singh Rana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
